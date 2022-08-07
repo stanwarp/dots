69d80502d1ac29711f4fae85a6ac84586470ff51
petrushi <pavlovinthesky@gmail.com>
2022-08-07 15:17:55 +0300

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,41 +1,41 @@
-local install_path = vim.fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
 	vim.fn.execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
 end
 
-local packer_group = vim.api.nvim_create_augroup("Packer", { clear = true })
+local packer_group = vim.api.nvim_create_augroup("packer", { clear = true })
 vim.api.nvim_create_autocmd(
-	"BufWritePost",
+	"bufwritepost",
 	{ command = "source <afile> | PackerCompile", group = packer_group, pattern = "init.lua" }
 )
 
 require("packer").startup(function(use)
-	use "wbthomason/packer.nvim"
+	use("wbthomason/packer.nvim")
 
-	use "andweeb/presence.nvim"
-	use { "catppuccin/nvim", as = "catppuccin" }
-	use "glepnir/dashboard-nvim"
-	use "hrsh7th/cmp-nvim-lsp"
-	use "hrsh7th/nvim-cmp"
-	use "jose-elias-alvarez/null-ls.nvim"
-	use "kyazdani42/nvim-web-devicons"
-	use "L3MON4D3/LuaSnip"
-	use "lewis6991/gitsigns.nvim"
-	use "neovim/nvim-lspconfig"
-	use "nvim-lua/plenary.nvim"
-	use "nvim-lualine/lualine.nvim"
-	use "nvim-telescope/telescope.nvim"
-	use "nvim-telescope/telescope-file-browser.nvim"
-	use "nvim-treesitter/nvim-treesitter"
-	use "onsails/lspkind-nvim"
-	use "ryanoasis/vim-devicons"
-	use "saadparwaiz1/cmp_luasnip"
-	use "simrat39/rust-tools.nvim"
-	use "tpope/vim-commentary"
-	use "williamboman/nvim-lsp-installer"
-	use "windwp/nvim-autopairs"
-	use "https://git.sr.ht/~whynothugo/lsp_lines.nvim"
+	use("andweeb/presence.nvim")
+	use({ "catppuccin/nvim", as = "catppuccin" })
+	use("glepnir/dashboard-nvim")
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/nvim-cmp")
+	use("jose-elias-alvarez/null-ls.nvim")
+	use("kyazdani42/nvim-web-devicons")
+	use("l3mon4d3/luasnip")
+	use("lewis6991/gitsigns.nvim")
+	use("neovim/nvim-lspconfig")
+	use("nvim-lua/plenary.nvim")
+	use("nvim-lualine/lualine.nvim")
+	use("nvim-telescope/telescope.nvim")
+	use("nvim-telescope/telescope-file-browser.nvim")
+	use("nvim-treesitter/nvim-treesitter")
+	use("onsails/lspkind-nvim")
+	use("ryanoasis/vim-devicons")
+	use("saadparwaiz1/cmp_luasnip")
+	use("simrat39/rust-tools.nvim")
+	use("tpope/vim-commentary")
+	use("williamboman/nvim-lsp-installer")
+	use("windwp/nvim-autopairs")
+	use("https://git.sr.ht/~whynothugo/lsp_lines.nvim")
 end)
 
 vim.o.clipboard = "unnamedplus"
@@ -52,21 +52,21 @@ vim.o.updatetime = 100
 
 vim.g.mapleader = " "
 
-vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
-vim.keymap.set("n", "<Leader>w", "<C-w>k")
-vim.keymap.set("n", "<Leader>a", "<C-w>h")
-vim.keymap.set("n", "<Leader>s", "<C-w>j")
-vim.keymap.set("n", "<Leader>d", "<C-w>l")
-vim.keymap.set("n", "<Leader>j", ":bprevious<CR>", { silent = true })
-vim.keymap.set("n", "<Leader>k", ":bnext<CR>", { silent = true })
-vim.keymap.set("n", "<Leader>q", ":bprevious<CR>:bdelete #<CR>", { silent = true })
-vim.keymap.set("n", "<Leader>/", ":nohlsearch<CR>", { silent = true })
-vim.keymap.set("n", "<Leader>y", ":%y<CR>")
+vim.keymap.set({ "n", "v" }, "<space>", "<nop>", { silent = true })
+vim.keymap.set("n", "<leader>w", "<c-w>k")
+vim.keymap.set("n", "<leader>a", "<c-w>h")
+vim.keymap.set("n", "<leader>s", "<c-w>j")
+vim.keymap.set("n", "<leader>d", "<c-w>l")
+vim.keymap.set("n", "<leader>j", ":bprevious<cr>", { silent = true })
+vim.keymap.set("n", "<leader>k", ":bnext<cr>", { silent = true })
+vim.keymap.set("n", "<leader>q", ":bprevious<cr>:bdelete #<cr>", { silent = true })
+vim.keymap.set("n", "<leader>/", ":nohlsearch<cr>", { silent = true })
+vim.keymap.set("n", "<leader>y", ":%y<cr>")
 vim.keymap.set("n", "k", "gk", { silent = true })
 vim.keymap.set("n", "j", "gj", { silent = true })
-vim.keymap.set("n", "<Leader>l", ":vsplit term://fish <CR>", { silent = true })
-vim.keymap.set("t", "<Leader><Esc>", "<C-\\><C-n>", { silent = true })
-vim.keymap.set("n", "<Leader>v", ":edit ~/.config/nvim/init.lua<CR>", { silent = true })
+vim.keymap.set("n", "<leader>l", ":vsplit term://bash <cr>", { silent = true })
+vim.keymap.set("t", "<leader><esc>", "<c-\\><c-n>", { silent = true })
+vim.keymap.set("n", "<leader>v", ":edit ~/.config/nvim/init.lua<cr>", { silent = true })
 
 local lang_maps = {
 	cpp = { build = "g++ % -o %:r", exec = "./%:r" },
@@ -79,126 +79,114 @@ local lang_maps = {
 	rust = { exec = "cargo run" },
 	arduino = {
 		build = "arduino-cli compile --fqbn arduino:avr:uno %:r",
-		exec = "arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno %:r",
+		exec = "arduino-cli upload -p /dev/ttyacm0 --fqbn arduino:avr:uno %:r",
 	},
 }
 for lang, data in pairs(lang_maps) do
 	if data.build ~= nil then
 		vim.api.nvim_create_autocmd(
-			"FileType",
-			{ command = "nnoremap <Leader>b :!" .. data.build .. "<CR>", pattern = lang }
+			"filetype",
+			{ command = "nnoremap <leader>b :!" .. data.build .. "<cr>", pattern = lang }
 		)
 	end
 	vim.api.nvim_create_autocmd(
-		"FileType",
-		{ command = "nnoremap <Leader>e :split<CR>:terminal " .. data.exec .. "<CR>", pattern = lang }
+		"filetype",
+		{ command = "nnoremap <leader>e :split<cr>:terminal " .. data.exec .. "<cr>", pattern = lang }
 	)
 end
-vim.api.nvim_create_autocmd("BufWritePre", {
+vim.api.nvim_create_autocmd("bufwritepre", {
 	command = "lua vim.lsp.buf.formatting_sync(nil, 1000)",
 	pattern = "*.cpp,*.css,*.go,*.h,*.html,*.js,*.json,*.jsx,*.lua,*.md,*.py,*.rs,*.ts,*.tsx,*.yaml",
 })
-vim.api.nvim_create_autocmd("InsertEnter", { command = "set norelativenumber", pattern = "*" })
-vim.api.nvim_create_autocmd("InsertLeave", { command = "set relativenumber", pattern = "*" })
-vim.api.nvim_create_autocmd("TermOpen", { command = "startinsert", pattern = "*" })
-vim.api.nvim_create_autocmd("BufWinEnter", { command = "set noexpandtab tabstop=2 shiftwidth=2", pattern = "*.rs" })
+vim.api.nvim_create_autocmd("insertenter", { command = "set norelativenumber", pattern = "*" })
+vim.api.nvim_create_autocmd("insertleave", { command = "set relativenumber", pattern = "*" })
+vim.api.nvim_create_autocmd("termopen", { command = "startinsert", pattern = "*" })
+vim.api.nvim_create_autocmd("bufwinenter", { command = "set noexpandtab tabstop=2 shiftwidth=2", pattern = "*.rs" })
 
-vim.cmd "sign define DiagnosticSignError text=● texthl=DiagnosticSignError"
-vim.cmd "sign define DiagnosticSignWarn text=● texthl=DiagnosticSignWarn"
-vim.cmd "sign define DiagnosticSignInfo text=● texthl=DiagnosticSignInfo"
-vim.cmd "sign define DiagnosticSignHint text=● texthl=DiagnosticSignHint"
+vim.cmd("sign define diagnosticsignerror text=● texthl=diagnosticsignerror")
+vim.cmd("sign define diagnosticsignwarn text=● texthl=diagnosticsignwarn")
+vim.cmd("sign define diagnosticsigninfo text=● texthl=diagnosticsigninfo")
+vim.cmd("sign define diagnosticsignhint text=● texthl=diagnosticsignhint")
 
-vim.diagnostic.config { virtual_text = false }
+vim.diagnostic.config({ virtual_text = false })
 
-require("presence"):setup {
-	neovim_image_text = "Neovim",
+require("presence"):setup({
+	neovim_image_text = "neovim",
 	presence_log_level = "error",
-	presence_editing_text = "Editing « %s »",
-	presence_file_explorer_text = "Browsing files",
-	presence_reading_text = "Reading  « %s »",
-	presence_workspace_text = "Working on « %s »",
-}
+	presence_editing_text = "editing « %s »",
+	presence_file_explorer_text = "browsing files",
+	presence_reading_text = "reading  « %s »",
+	presence_workspace_text = "working on « %s »",
+})
 
 vim.g.catppuccin_flavour = "mocha"
-vim.cmd "colorscheme catppuccin"
+vim.cmd("colorscheme catppuccin")
 
-local db = require "dashboard"
+local db = require("dashboard")
 db.custom_header = {
-	"",
-	"",
-	"",
-	"",
-	" ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗",
-	" ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║",
-	" ██╔██╗ ██║ ██║     ██║   ██║ ██║   ██║ ██║ ██╔████╔██║",
-	" ██║╚██╗██║ ██║     ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║",
-	" ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║",
-	" ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝",
-	"",
-	"",
-	"",
+	"zdarova",
 }
 db.custom_center = {
 	{
 		icon = " ",
-		desc = "New File            ",
-		action = "DashboardNewFile",
-		shortcut = "SPC o",
+		desc = "new file            ",
+		action = "dashboardnewfile",
+		shortcut = "spc o",
 	},
 	{
 		icon = " ",
-		desc = "Browse Files        ",
-		action = "Telescope file_browser",
-		shortcut = "SPC n",
+		desc = "browse files        ",
+		action = "telescope file_browser",
+		shortcut = "spc n",
 	},
 	{
 		icon = " ",
-		desc = "Find File           ",
-		action = "Telescope find_files",
-		shortcut = "SPC f",
+		desc = "find file           ",
+		action = "telescope find_files",
+		shortcut = "spc f",
 	},
 	{
 		icon = " ",
-		desc = "Configure Neovim    ",
+		desc = "configure neovim    ",
 		action = "edit ~/.config/nvim/lua/init.lua",
-		shortcut = "SPC v",
+		shortcut = "spc v",
 	},
 	{
 		icon = " ",
-		desc = "Exit Neovim              ",
+		desc = "exit neovim              ",
 		action = "quit",
 	},
 }
-vim.keymap.set("n", "<Leader>o", ":DashboardNewFile<CR>", { silent = true })
+vim.keymap.set("n", "<leader>o", ":dashboardnewfile<cr>", { silent = true })
 
-local luasnip = require "luasnip"
-local cmp = require "cmp"
-cmp.setup {
+local luasnip = require("luasnip")
+local cmp = require("cmp")
+cmp.setup({
 	snippet = {
 		expand = function(args)
 			luasnip.lsp_expand(args.body)
 		end,
 	},
-	mapping = cmp.mapping.preset.insert {
-		["<C-Space>"] = cmp.mapping.complete(),
-		["<CR>"] = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Replace, select = true },
-		["<Tab>"] = cmp.mapping(function(fallback)
+	mapping = cmp.mapping.preset.insert({
+		["<c-space>"] = cmp.mapping.complete(),
+		["<cr>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.replace, select = true }),
+		["<tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
 			else
 				fallback()
 			end
 		end, { "i", "s" }),
-		["<S-Tab>"] = cmp.mapping(function(fallback)
+		["<s-tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
 			else
 				fallback()
 			end
 		end, { "i", "s" }),
-	},
+	}),
 	sources = { { name = "nvim_lsp" }, { name = "luasnip" } },
-}
+})
 
 local servers = {
 	"bashls",
@@ -216,13 +204,15 @@ local has_formatter = { "gopls", "html", "rust_analyzer", "sumneko_lua", "tsserv
 for _, name in pairs(servers) do
 	local found, server = require("nvim-lsp-installer").get_server(name)
 	if found and not server:is_installed() then
-		print("Installing " .. name)
+		print("yep")
+		print("installing " .. name)
+		print("yyeeeapp")
 		server:install()
 	end
 end
 local setup_server = {
 	sumneko_lua = function(opts)
-		opts.settings = { Lua = { diagnostics = { globals = { "vim" } } } }
+		opts.settings = { lua = { diagnostics = { globals = { "vim" } } } }
 	end,
 }
 require("nvim-lsp-installer").on_server_ready(function(server)
@@ -230,9 +220,9 @@ require("nvim-lsp-installer").on_server_ready(function(server)
 		on_attach = function(client, bufnr)
 			vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 			local opts = { buffer = bufnr }
-			vim.keymap.set("n", "<Leader>h", vim.lsp.buf.hover, opts)
-			vim.keymap.set("n", "<Leader>i", vim.lsp.buf.definition, opts)
-			vim.keymap.set("n", "<Leader>r", vim.lsp.buf.rename, opts)
+			vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover, opts)
+			vim.keymap.set("n", "<leader>i", vim.lsp.buf.definition, opts)
+			vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, opts)
 			local should_format = true
 			for _, value in pairs(has_formatter) do
 				if client.name == value then
@@ -251,8 +241,8 @@ require("nvim-lsp-installer").on_server_ready(function(server)
 	server:setup(opts)
 end)
 
-local null_ls = require "null-ls"
-null_ls.setup {
+local null_ls = require("null-ls")
+null_ls.setup({
 	sources = {
 		null_ls.builtins.diagnostics.eslint_d,
 		null_ls.builtins.formatting.autopep8,
@@ -262,9 +252,9 @@ null_ls.setup {
 		null_ls.builtins.formatting.rustfmt,
 		null_ls.builtins.formatting.stylua,
 	},
-}
+})
 
-require("gitsigns").setup {
+require("gitsigns").setup({
 	signs = {
 		add = { text = "+" },
 		change = { text = "~" },
@@ -272,10 +262,10 @@ require("gitsigns").setup {
 		topdelete = { text = "‾" },
 		changedelete = { text = "~" },
 	},
-}
+})
 
 local cp = require("catppuccin.palettes.init").get_palette()
-local custom_catppuccin = require "lualine.themes.catppuccin"
+local custom_catppuccin = require("lualine.themes.catppuccin")
 custom_catppuccin.normal.b.bg = cp.surface0
 custom_catppuccin.normal.c.bg = cp.base
 custom_catppuccin.insert.b.bg = cp.surface0
@@ -286,7 +276,7 @@ custom_catppuccin.inactive.a.bg = cp.base
 custom_catppuccin.inactive.b.bg = cp.base
 custom_catppuccin.inactive.b.fg = cp.surface0
 custom_catppuccin.inactive.c.bg = cp.base
-require("lualine").setup {
+require("lualine").setup({
 	options = {
 		theme = custom_catppuccin,
 		component_separators = "|",
@@ -318,10 +308,10 @@ require("lualine").setup {
 			},
 		},
 	},
-}
+})
 
-local telescope = require "telescope"
-telescope.setup {
+local telescope = require("telescope")
+telescope.setup({
 	defaults = {
 		mappings = { n = { ["o"] = require("telescope.actions").select_default } },
 		initial_mode = "normal",
@@ -329,13 +319,13 @@ telescope.setup {
 		file_ignore_patterns = { ".git/", "node_modules/", "target/" },
 	},
 	extensions = { file_browser = { hidden = true } },
-}
-telescope.load_extension "file_browser"
-vim.keymap.set("n", "<Leader>n", telescope.extensions.file_browser.file_browser)
-vim.keymap.set("n", "<Leader>f", require("telescope.builtin").find_files)
-vim.keymap.set("n", "<Leader>t", require("telescope.builtin").treesitter)
+})
+telescope.load_extension("file_browser")
+vim.keymap.set("n", "<leader>n", telescope.extensions.file_browser.file_browser)
+vim.keymap.set("n", "<leader>f", require("telescope.builtin").find_files)
+vim.keymap.set("n", "<leader>t", require("telescope.builtin").treesitter)
 
-require("nvim-treesitter.configs").setup {
+require("nvim-treesitter.configs").setup({
 	ensure_installed = {
 		"bash",
 		"cpp",
@@ -351,13 +341,13 @@ require("nvim-treesitter.configs").setup {
 		"yaml",
 	},
 	highlight = { enable = true },
-}
+})
 
-require("rust-tools").setup {}
+require("rust-tools").setup({})
 
-vim.keymap.set({ "n", "v" }, "<Leader>c", ":Commentary<CR>", { silent = true })
+vim.keymap.set({ "n", "v" }, "<leader>c", ":commentary<cr>", { silent = true })
 
-require("nvim-autopairs").setup {}
+require("nvim-autopairs").setup({})
 
-require("lsp_lines").setup {}
-vim.keymap.set("n", "<Leader>x", require("lsp_lines").toggle)
+require("lsp_lines").setup({})
+vim.keymap.set("n", "<leader>x", require("lsp_lines").toggle)
