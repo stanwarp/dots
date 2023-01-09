815c568ec688b9c6265388f84799a8a80e73d50f
stanwarp <pavlovinthesky@gmail.com>
2023-01-09 06:01:55 +0300

    Create config.fish

---
+++ fish/config.fish
@@ -0,0 +1,48 @@
+set fish_greeting ""
+
+set -gx TERM xterm-256color
+if type -q exa
+  alias ls "exa --icons"
+  alias ll "exa -l -g --icons"
+  alias lla "ll -a"
+end
+alias v "nvim"
+alias t "tmux attach"
+alias c "clear"
+# theme
+# set -g theme_color_scheme terminal-dark
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_display_user yes
+set -g theme_hide_hostname no
+set -g theme_hostname always
+alias c codium
+alias g git
+command -qv nvim && alias vim nvim
+
+set -gx EDITOR nvim
+
+set -gx PATH bin $PATH
+set -gx PATH ~/bin $PATH
+set -gx PATH ~/.local/bin $PATH
+
+# NodeJS
+set -gx PATH node_modules/.bin $PATH
+
+# Go
+set -g GOPATH $HOME/go
+set -gx PATH $GOPATH/bin $PATH
+
+# NVM
+function __check_rvm --on-variable PWD --description 'Do nvm stuff'
+  status --is-command-substitution; and return
+
+  if test -f .nvmrc; and test -r .nvmrc;
+    nvm use
+  else
+  end
+end
+
+set LOCAL_CONFIG (dirname (status --current-filename))/config-local.fish
+if test -f $LOCAL_CONFIG
+  source $LOCAL_CONFIG
+end
